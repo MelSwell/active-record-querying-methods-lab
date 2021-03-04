7c3f6a64dffda5971a834ccfa17e491e999dcc5d
Seth M Cohen <smcohen82@gmail.com>
2021-03-04 16:42:50 -0500

    commit tree 9f10871e563a0661a08a93718012a889ac3d8969

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,33 +1,31 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    ratings = Show.all.map { |show| show.rating }
-    ratings.sort { |show_a, show_b| show_b <=> show_a }.first
+    Show.maximum(:rating)
   end
 
   def self.most_popular_show
-    Show.all.select { |show| show.rating == self.highest_rating }.first
+    Show.where("rating = ?", self.highest_rating).first
   end
 
   def self.lowest_rating
-    ratings = Show.all.map { |show| show.rating }
-    ratings.sort { |show_a, show_b| show_a <=> show_b }.first
+    Show.minimum(:rating)
   end
 
   def self.least_popular_show
-    Show.all.select { |show| show.rating == self.lowest_rating }.first
+    Show.where("rating = ?", self.lowest_rating).first
   end
 
   def self.ratings_sum
-    Show.all.map { |show| show.rating }.sum
+    Show.sum("rating")
   end
 
   def self.popular_shows
-    Show.all.select { |show| show.rating > 5 }
+    Show.where("rating > 5")
   end
 
   def self.shows_by_alphabetical_order
-    Show.all.sort { |show_a, show_b| show_a.name <=> show_b.name }
+    Show.order("name")
   end
 
 end
